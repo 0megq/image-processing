27a9b977c56f67b34fef46f3c3e9096ce71e2d3c
0megq <nvatanshenas55@gmail.com>
2025-11-20 23:19:30 -0800

    tkinter ui a little better: time to connect the two parts

--- test1.py
+++ test1.py
@@ -5,12 +5,12 @@ from PIL import Image, ImageTk
 
 class DisplayFrame(tk.Frame):
 	def __init__(self, parent):
-		width = 400
-		height = 400
+		width = 800
+		height = 600
 		super().__init__(parent, width=width, height=height, bg='lightblue')
-		
+		self.right = 0
 		self.pack_propagate(False)
-		self.pack(side=tk.LEFT, padx=10, pady=10, fill=tk.BOTH, expand=True)
+		self.pack(side=tk.LEFT, padx=5, pady=5, fill=tk.BOTH, expand=True)
 		self.bind("<Configure>", self.on_resize)
 
 		#Load the image
@@ -18,48 +18,65 @@ class DisplayFrame(tk.Frame):
 
 		#Rearrange colors BGR -> RGB
 		self.src_img = cv2.cvtColor(self.src_img, cv2.COLOR_BGR2RGB)
-		scale_factor = (width - 20) / self.src_img.shape[1]
-		img = cv2.resize(self.src_img, None, fx=scale_factor, fy=scale_factor, interpolation=cv2.INTER_AREA)
+		self.scale_factor = (height) / self.src_img.shape[0]
+		img = cv2.resize(self.src_img, None, fx=self.scale_factor, fy=self.scale_factor, interpolation=cv2.INTER_AREA)
 		# cv2.imshow('hello', img)
 
 		# create tk image
 		im = Image.fromarray(img)
-		imgtk = ImageTk.PhotoImage(image=im)
+		self.imgtk = ImageTk.PhotoImage(image=im)
 
 		# Add a label to the left Frame
-		self.img_label = tk.Label(self, image=imgtk)
-		self.img_label.pack(pady=20)
+		self.img_label = tk.Label(self, image=self.imgtk, bg='lightblue')
+		self.img_label.pack()
+		self.img_label.bind("<Motion>", self.on_img_mouse_move)
+		# self.img_label.bind("<Enter>", self.on_img_entered)
+		# self.img_label.bind("<Leave>", self.on_img_exited)
 
 		# Add a button to the left Frame
-		left_button = tk.Button(self, text="Left Button", command=lambda: print("Left Button clicked!"))
-		left_button.pack(pady=10)
+		# left_button = tk.Button(self, text="Left Button", command=lambda: print("Left Button clicked!"))
+		# left_button.pack(pady=10)
 
 	def on_resize(self, event):
 		width = event.width
 		height = event.height
 	
-		scale_factor = (width - 20) / self.src_img.shape[1]
-		img = cv2.resize(self.src_img, None, fx=scale_factor, fy=scale_factor, interpolation=cv2.INTER_AREA)
+		self.scale_factor = (height) / self.src_img.shape[0]
+		img = cv2.resize(self.src_img, None, fx=self.scale_factor, fy=self.scale_factor, interpolation=cv2.INTER_AREA)
 
 		# create tk image
 		im = Image.fromarray(img)
-		imgtk = ImageTk.PhotoImage(image=im)
-		self.img_label
+		self.imgtk = ImageTk.PhotoImage(image=im)
+		self.img_label.configure(image=self.imgtk)
+		self.right.size_lbl.config(text="Size: %d x %d" % (self.src_img.shape[1], self.src_img.shape[0]))
+
+	def on_img_mouse_move(self, event):
+		self.right.mouse_pos_lbl.config(text="Mouse Pos: (%d, %d)" % (event.x / self.scale_factor, event.y / self.scale_factor))
+
+
+	# def adjust_zoom(self):
+
 
 
 class UIFrame(tk.Frame):
 	def __init__(self, parent):
-		super().__init__(parent, width=200, height=200, bg='lightgreen')
+		super().__init__(parent, width=150, height=200, bg='lightgreen')
 		# Create the right Frame
-		self.pack(side=tk.LEFT, padx=10, pady=10, fill=tk.BOTH)
+		self.pack_propagate(False)
+		self.pack(side=tk.LEFT, padx=5, pady=5, fill=tk.BOTH)
 
 		# Add a label to the right Frame
-		right_label = tk.Label(self, text="Right Frame", bg='lightgreen')
-		right_label.pack(pady=20)
+		right_label = tk.Label(self, text="Controls", bg='lightgreen')
+		right_label.pack(pady=5, side="top")
 
 		# Add a button to the right Frame
 		right_button = tk.Button(self, text="Right Button", command=lambda: print("Right Button clicked!"))
-		right_button.pack(pady=10)
+		right_button.pack(pady=10, padx=10)
+		
+		self.size_lbl = tk.Label(self, text="Image size: ", bg='lightgreen')
+		self.size_lbl.pack(padx=5, side="bottom", anchor="w")
+		self.mouse_pos_lbl = tk.Label(self, text="Mouse Pos: ", bg='lightgreen')
+		self.mouse_pos_lbl.pack(pady=5, padx=5, side="bottom", anchor="w")
 	
 
 # Create the main application window
@@ -68,6 +85,7 @@ root.title("Tube Helper")
 
 left_frame = DisplayFrame(root)
 right_frame = UIFrame(root)
+left_frame.right = right_frame
 
 # Start the Tkinter event loop
 root.mainloop()
