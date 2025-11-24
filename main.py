384b94ea9ab3033b8ac847c063babf764b8ca31e
0megq <nvatanshenas55@gmail.com>
2025-11-23 18:04:19 -0800

    added zoom and panning

--- main.py
+++ main.py
@@ -81,8 +81,14 @@ def update_display(param):
 		cv2.namedWindow(originalName,  cv2.WINDOW_AUTOSIZE)
 		cv2.setMouseCallback(originalName, on_mouse, param)
 
-	cv2.imshow(outlineName, param["outl"])
-	cv2.imshow(originalName, param["orig"])
+	cv2.imshow(outlineName, zoom_image(param["outl"], param["zoom"], param["top_left"]))
+	cv2.imshow(originalName, zoom_image(param["orig"], param["zoom"], param["top_left"]))
+
+def zoom_image(img, zoom, top_left):
+	h, w = img.shape[0], img.shape[1]
+	cropped = img[int(top_left[1]):int(top_left[1] + h/zoom), int(top_left[0]):int(top_left[0] + w/zoom)]
+	scaled_up = cv2.resize(cropped, (w, h), interpolation=cv2.INTER_NEAREST)
+	return scaled_up
 
 
 def update_thresh1(val, param):
@@ -114,8 +120,11 @@ def is_window_open(name: str) -> bool:
         return False
 
 # image analysis, window mouse callback
-def on_mouse(event, x, y, flags, param):
+def on_mouse(event, px, py, flags, param):
 	global originalName, outlineName
+	# account for zoom and pan
+	x, y = param["top_left"][0] + px / param["zoom"], param["top_left"][1] + py / param["zoom"]
+	param["mouse_pos"] = (x, y)
 	# try to place a new point
 	if event == cv2.EVENT_LBUTTONDOWN:
 		param["lmb_down"] = True
@@ -171,16 +180,55 @@ def on_mouse(event, x, y, flags, param):
 		param["lmb_down"] = False
 		update_display(param)
 	elif event == cv2.EVENT_MOUSEMOVE:
-		if not param["lmb_down"]: return
-		if param["selected_point"] == -1: return
+		if param["lmb_down"] and param["selected_point"] != -1:
+			nearest = nearest_edge(x, y, param["edges"])
+			if nearest is None:
+				print("No edge pixels found.")
+				return
+			point_idx = param["selected_point"]
+			param["points"][point_idx] = nearest
+			update_display(param)
+		elif param["pan_down"]:
+			width = param["orig_fr"].shape[1]
+			height = param["orig_fr"].shape[0]
+			zoom = param["zoom"]
+			delta_x = px - param["pan_down_pos"][0]
+			delta_y = py - param["pan_down_pos"][1]
+			top_left_x, top_left_y = (param["top_left_prev"][0] - delta_x / zoom, param["top_left_prev"][1] - delta_y / zoom)
+			top_left_x = min(max(top_left_x, 0), width - width / zoom)
+			top_left_y = min(max(top_left_y, 0), height - height / zoom)
+			new_top_left = (int(top_left_x), int(top_left_y))
+			old_top_left = param["top_left"]
+			param["top_left"] = new_top_left
+			# print(delta_x, delta_y)
+			# print((new_top_left[0] - old_top_left[0]) ** 2 + (new_top_left[1] - old_top_left[1]) ** 2)
 
-		nearest = nearest_edge(x, y, param["edges"])
-		if nearest is None:
-			print("No edge pixels found.")
-			return
-		point_idx = param["selected_point"]
-		param["points"][point_idx] = nearest
+			update_display(param)
+	elif event == cv2.EVENT_MOUSEWHEEL:
+		delta = flags >> 16
+		# max zoom of 2, min zoom of 1 (AKA no zoom)
+		old_zoom = param["zoom"]
+		new_zoom = min(max(old_zoom * 1.1 ** (delta / 120), 1), 10)
+		width = param["orig_fr"].shape[1]
+		height = param["orig_fr"].shape[0]
+		# zoom in/out from the center, and then clamp top_left to a valid spot
+		
+		top_left_x = x - px / new_zoom
+		top_left_y = y - py / new_zoom
+		# top_left_x -= (width/new_zoom - width/old_zoom) / 2
+		# top_left_y = y - py / new_zoom
+
+		top_left_x = min(max(top_left_x, 0), width - width / new_zoom)
+		top_left_y = min(max(top_left_y, 0), height - height / new_zoom)
+		param["top_left"] = (top_left_x, top_left_y)
+		param["zoom"] = new_zoom
 		update_display(param)
+	elif event == cv2.EVENT_MBUTTONDOWN:
+		param["top_left_prev"] = (param["top_left"][0], param["top_left"][1])
+		param["pan_down_pos"] = (px, py)
+		param["pan_down"] = True
+	elif event == cv2.EVENT_MBUTTONUP:
+		param["pan_down"] = False
 
 
 # provides image analysis of a single image
@@ -200,10 +248,6 @@ def analyze_segment(img) -> List:
 	# draw every edge pixel as a thin green point
 	edge_y, edge_x = np.where(edges == 255)
 	disp_outl[edge_y, edge_x] = (0, 255, 0)		# BGR green
-	
-	# Create windows
-	cv2.namedWindow(outlineName,  cv2.WINDOW_AUTOSIZE)
-	cv2.namedWindow(originalName,  cv2.WINDOW_AUTOSIZE)
 
 	# Pass the two mutable images in a dict so the callback can modify them
 	callback_data = {
@@ -211,18 +255,17 @@ def analyze_segment(img) -> List:
 		"thresh1": 20, "thresh2": 150,
 		"points": [(-1, -1)] * 4,
 		"lmb_down": False,
-		"selected_point": -1
+		"selected_point": -1,
+		"mouse_pos": (0, 0),
+		"zoom": 1,
+		"top_left": (0, 0),
+		"pan_down": False,
+		"pan_down_pos": (0, 0),
+		"top_left_prev": (0, 0)
 	}
-	cv2.setMouseCallback(outlineName, on_mouse, callback_data)
-	cv2.setMouseCallback(originalName, on_mouse, callback_data)
-	
-	# create trackbars for canny thresholds
-	cv2.createTrackbar('threshold 1', outlineName, canny_thr[0], 500, lambda val: update_thresh1(val, callback_data))
-	cv2.createTrackbar('threshold 2', outlineName, canny_thr[1], 500, lambda val: update_thresh2(val, callback_data))
 
-	# Show the initial frames
-	cv2.imshow(outlineName,  disp_outl)
-	cv2.imshow(originalName,  disp_orig)
+	update_display(callback_data)
+
 
 	wait_until_tk_event(next_event, callback_data)
 	cv2.destroyAllWindows()
@@ -299,19 +342,25 @@ cv_thread = threading.Thread(target=run_cv, daemon=True)
 cv_thread.start()
 root = tk.Tk()
 root.title("Controls")
-root.geometry("400x400")
+root.geometry("200x400")
 root.pack_propagate(True)
 tk.Label(root, text="Controls").pack(pady=10, padx=10)
 tk.Button(root, text="Reopen", command=lambda : update_event.set()).pack(padx=10, pady=5)
 tk.Button(root, text="Next", command=lambda : next_event.set()).pack(padx=10, pady=5)
+mouse_pos_lbl = tk.Label(root, text="No data yet")
+mouse_pos_lbl.pack()
+selected_pos_lbl = tk.Label(root, text="No point selected")
+selected_pos_lbl.pack()
 tk.Button(root, text="Quit", command=lambda : root.destroy()).pack(padx=10, pady=5)
-lbl = tk.Label(root, text="No data yet")
-lbl.pack()
 def poll():
 	try:
 		while True:                  # drain all available messages
 			info = msg_q.get_nowait()
-			lbl.config(text=info["points"])
+			text = "Mouse Pos (%d, %d)" % (info["mouse_pos"][0] / scale_factor, info["mouse_pos"][1] / scale_factor)
+			mouse_pos_lbl.config(text=text)
+			if info["selected_point"] != -1:
+				text = "Selected Point Pos (%d, %d)" % (info["points"][info["selected_point"]][0] / scale_factor, info["points"][info["selected_point"]][1] / scale_factor)
+				selected_pos_lbl.config(text=text)
 	except queue.Empty:
 		root.after(50, poll)
 
